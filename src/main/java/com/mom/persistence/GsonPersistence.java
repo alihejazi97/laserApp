4670367494c505d162e9d5529834c56613f2b0ac
Ali Hejazi <seyed.ali.hejazi.35@gmail.com>
2019-03-24 11:28:11 +0430

    :D

--- src/main/java/com/mom/persistence/GsonPersistence.java
+++ src/main/java/com/mom/persistence/GsonPersistence.java
@@ -15,56 +15,17 @@ import java.util.List;
 
 
 public class GsonPersistence {
-    static CameraControl cameraControl = CameraControl.getInstance();
 
-    public static void persist(List objects) {
-        File file = new File("./setting.json");
-        try{if (file.createNewFile()){
-            System.out.println("lets create the file.");
-        }
-        else
-        {
-            System.out.println("file has been created.");
-        }
-            Gson gson = new Gson();
-            FileWriter writer = new FileWriter(file);
-            gson.toJson(objects,writer);
-            writer.close();
-        }
-        catch (IOException e){}
-    }
-    public static List load(){
-        ArrayList<ColorBound> bounds = new ArrayList<>();
-        File file = new File("./setting.json");
-        try{if (file.createNewFile()){
-            System.out.println("fuck u we dont have this file");
-        }
-        else
-        {
-            System.out.println("file has been created.");
-            Gson gson = new Gson();
-            FileReader reader = new FileReader(file);
-            ColorBound [] colorBounds =  gson.fromJson(reader, ColorBound[].class);
-            if (colorBounds == null)
-                return bounds;
-            for (int i = 0; i < colorBounds.length; i++) {
-                bounds.add(colorBounds[i]);
-            }
-            reader.close();
-        }
-        }
-        catch (IOException e){}
-        return bounds;
-    }
+    private static final int GUN_NUMBER_DEFAULT = 1;
+    private static final int TARGET_NUMBER_DEFAULT = 1;
+
+    static CameraControl cameraControl = CameraControl.getInstance();
 
     public static void persist2(List objects) {
         File file = new File("./settingTarget.json");
-        try{if (file.createNewFile()){
-            System.out.println("lets create the file.");
-        }
-        else
-        {
-            System.out.println("file has been created.");
+        try{
+            if (file.createNewFile()){
+            System.out.println("creating the file.");
         }
             GsonBuilder gsonBuilder  = new GsonBuilder();
             gsonBuilder.excludeFieldsWithModifiers(java.lang.reflect.Modifier.TRANSIENT);
@@ -75,47 +36,42 @@ public class GsonPersistence {
         }
         catch (IOException e){}
     }
+
+    public static void makeDefaultTargets(List<Target> targets){
+        final List<String> cameraNames = cameraControl.getCameraNames();
+        Target.TARGET_NUMBER = TARGET_NUMBER_DEFAULT;
+        Target.GUN_NUMBER = GUN_NUMBER_DEFAULT;
+        Target.camDescriptor = new String();
+        for (int i = 0; i < Target.TARGET_NUMBER; i++) {
+            Target target = new Target();
+            target.name = Integer.toString(i);
+            if (!cameraNames.isEmpty())
+                target.webCamName = cameraNames.get(0);
+            else
+                target.webCamName = new String();
+            target.gunId = 0;
+            targets.add(target);
+        }
+    }
+
     public static List load2(){
         ArrayList<Target> targets = new ArrayList<>();
         File file = new File("./settingTarget.json");
-        try{if (file.createNewFile()){
-            System.out.println("fuck u we dont have this file");
-            final List<String> cameraNames = cameraControl.getCameraNames();
-            Target.TARGET_NUMBER = 1;
-            Target.GUN_NUMBER = 1;
-            Target.camDescriptor = "";
-            for (int i = 0; i < Target.TARGET_NUMBER; i++) {
-                Target target = new Target();
-                target.name = Integer.toString(i);
-                if (!cameraNames.isEmpty())
-                    target.webCamName = cameraNames.get(0);
-                else target.webCamName = "";
-                target.gunId = 0;
-                targets.add(target);
-            }
+        try {
+            if (file.createNewFile()){
+                System.out.println("file has been created.");
+                makeDefaultTargets(targets);
         }
         else
         {
-            System.out.println("file has been created.");
+            System.out.println("file has been loaded.");
             GsonBuilder gsonBuilder  = new GsonBuilder();
             gsonBuilder.excludeFieldsWithModifiers(java.lang.reflect.Modifier.TRANSIENT);
             Gson gson = gsonBuilder.create();
             FileReader reader = new FileReader(file);
             Target[] Targets =  gson.fromJson(reader, Target[].class);
             if (Targets == null || Targets.length == 0){
-                final List<String> cameraNames = cameraControl.getCameraNames();
-                Target.TARGET_NUMBER = 1;
-                Target.GUN_NUMBER = 1;
-                Target.camDescriptor = "";
-                for (int i = 0; i < Target.TARGET_NUMBER; i++) {
-                    Target target = new Target();
-                    target.name = Integer.toString(i);
-                    if (!cameraNames.isEmpty())
-                        target.webCamName = cameraNames.get(0);
-                    else target.webCamName = "";
-                    target.gunId = 0;
-                    targets.add(target);
-                }
+                makeDefaultTargets(targets);
             }
             else{
                 for (int i = 0; i < Targets.length; i++) {
